d2f7e35cbdd6f06c9691a340894d5b8c8eaff549
rodol <rodol@Rodolphe>
2019-11-17 20:18:21 +0100

    Update

--- ProjectLO02/src/projet/Joueur.java
+++ ProjectLO02/src/projet/Joueur.java
@@ -2,4 +2,25 @@ package projet;
 
 public class Joueur {
 
+	private String pseudo="Joueur00";
+	private Boolean type_joueur=false;
+	
+	public Joueur(String name, Boolean type) {
+		this.pseudo=name;
+		this.type_joueur = type;
+		
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(this.pseudo);
+		sb.append(";");
+		sb.append(this.type_joueur);
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		Joueur player = new Joueur("Rodolphe",true);
+		System.out.println(player);
+	}
 }
